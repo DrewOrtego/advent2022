229490a35a3684e4c1c9675d1321274f8a124d85
drewortego <ortegoa@gmail.com>
2022-12-05 22:21:06 -0800

    day 5 pt 1 incomplete

---
+++ day_5/part_1/day_5.py
@@ -0,0 +1,55 @@
+from re import search
+from string import ascii_uppercase
+
+file_name = 'input.txt'
+
+def get_crates():
+    """ Build data structure for the stacks of crates"""
+    crates = {}
+    with open(file_name, 'r') as file_data:
+        for line in file_data.readlines():
+            if line != '':
+                for e, c in enumerate(line):  # e counts the char position
+                    if c in ascii_uppercase:
+                        if (e%4)+(e//4) not in crates:
+                            crates.setdefault((e%4)+(e//4), list())
+                        crates[(e%4)+(e//4)].append(c)
+            else:
+                break
+    return crates
+
+def get_instructions():
+    """ Use Regex to get instructions on how to move crates"""
+    instructions = []
+    exp = r'move ([0-9]) from ([0-9]) to ([0-9])'
+
+    with open(file_name, 'r') as file_data:
+        for line in file_data.readlines():
+            result = search(exp, line.strip())
+            if result:
+                instructions.append([int(g) for g in result.groups()])
+
+    return instructions
+
+def move_crates(crates, instructions):
+    for i in instructions:
+        amount = i[0]
+        source = i[1]
+        destination = i[2]
+
+        for _ in range(amount):
+            if crates[source]:
+                crates[destination].insert(0,crates[source].pop(0))
+
+def print_top_crates(crates):
+    message = ''
+    for n in range(1,len(crates)+1):
+        if crates[n]:
+            message += crates[n][0]
+    print(message)
+
+if __name__ == "__main__":
+    crates = get_crates()
+    instructions = get_instructions()
+    move_crates(crates, instructions)
+    print_top_crates(crates)
